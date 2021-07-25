c3e856d3d85a8bf21faebf999c57a40732e0dceb
Lucas Febatis <lfebatis@gmail.com>
2021-07-25 13:18:08 -0300

    Aplicando cor primaria

--- lib/app.dart
+++ lib/app.dart
@@ -19,7 +19,7 @@ class App extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
